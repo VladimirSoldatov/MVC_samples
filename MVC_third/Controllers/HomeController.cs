87575c6eaeba23bb848ba069f60048ccb6081cef
Vladimir <cbb110@rambler.ru>
2024-09-22 12:59:49 +0300

    Forms

---
+++ MVC_third/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using MVC_third.Models;
+using System.Diagnostics;
+
+namespace MVC_third.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task Index()
+        {
+            string content = @"<form method='post'>
+                <label>Name:</label><br />
+                <input name='name' /><br />
+                <label>Age:</label><br />
+                <input type='number' name='age' /><br />
+                <input type='submit' value='Send' />
+            </form>";
+            Response.ContentType = "text/html;charset=utf-8";
+            await Response.WriteAsync(content);
+        }
+        [HttpPost]
+        public string Index(string name, int age) => $"{name}: {age}";
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
